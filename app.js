236e504b46522aa5f72554586d2595e7a24164ea
Mukesh Kumar Singh <kumarsinghmukesh237@gmail.com>
2026-01-16 02:59:13 +0530

    cloudinary changes

--- app.js
+++ app.js
@@ -42,9 +42,6 @@ app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
 
-
-
-
 const store = MongoStore.create({
   mongoUrl :dbUrl,
   crypto:{
