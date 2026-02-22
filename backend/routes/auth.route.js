f12cb30f4d7aa8b600b06b23aa0105a8b6889f41
Mukesh9116 <146856075+Mukesh9116@users.noreply.github.com>
2026-02-22 21:55:48 +0530

    Fix typo in signup route handler name

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -3,8 +3,9 @@ import { login, logOut, sighUp } from "../controllers/auth.controller.js"
 
 const authRouter = express.Router()
 
-authRouter.post("/signup",signUp)
+authRouter.post("/signup",sighUp)
 authRouter.post("/login",login)
 authRouter.post("/logout",logOut)
 
 export default authRouter
+
